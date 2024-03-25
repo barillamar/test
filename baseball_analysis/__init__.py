2519ee7f7f0653e4dd5f75c8927e3520bb6f23d6
barillamar <127573905+barillamar@users.noreply.github.com>
2024-03-25 05:38:18 -0500

    Update __init__.py

--- baseball_analysis/__init__.py
+++ baseball_analysis/__init__.py
@@ -1,6 +1,7 @@
 #from .zone_analysis import fetch_gamepks(), get_batting_order_of_game(), convert_names_to_mlb_ids(), get_swing_and_miss_rate_by_zone(), get_called_strike_rate_by_zone(), get_slugging_percentage_by_zone(), get_on_base_percentage_by_zone(), get_batting_average_by_zone()
 from .zone_analysis import fetch_gamepks
 from .zone_analysis import get_batting_order_of_game
+from .zone_analysis import convert_names_to_mlb_ids
 from .zone_analysis import get_swing_and_miss_rate_by_zone
 from .zone_analysis import get_called_strike_rate_by_zone
 from .zone_analysis import get_slugging_percentage_by_zone
